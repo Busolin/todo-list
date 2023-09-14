1efd0e3ce8560fda3acd1e105fd7df7b21278771
Busolinkz <victorbusolin@alunos.utfpr.edu.br>
2023-09-14 11:39:29 -0300

    fix: changed radioButton from view to button

--- src/Components/RadioButton/index.tsx
+++ src/Components/RadioButton/index.tsx
@@ -1,24 +1,27 @@
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import Image from "react-native-remote-svg";
 import { styles } from "./style";
 
 export interface RadioButtonProps {
   selected: boolean;
+  onComplete: () => void;
 }
 
-export function RadioButton({ selected }: RadioButtonProps) {
+export function RadioButton({ selected, onComplete }: RadioButtonProps) {
   return (
     <View>
-      <View style={styles.radioButton}>
-        {selected ? (
-          <View style={styles.radioButtonDone}>
-            <Image
-              source={require("./checked.svg")}
-              style={styles.radioButtonImage}
-            />
-          </View>
-        ) : null}
-      </View>
+      <TouchableOpacity onPress={onComplete}>
+        <View style={styles.radioButton}>
+          {selected ? (
+            <View style={styles.radioButtonDone}>
+              <Image
+                source={require("./checked.svg")}
+                style={styles.radioButtonImage}
+              />
+            </View>
+          ) : null}
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
